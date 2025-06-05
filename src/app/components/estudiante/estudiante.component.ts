1577ab940d5c686df1c00cf3ba91a07bd55fe776
Hanny Paz <hwpazz@gmail.com>
2025-06-05 15:04:15 -0500

    agregamos Tailwind

--- src/app/components/estudiante/estudiante.component.ts
+++ src/app/components/estudiante/estudiante.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Estudiante } from '../../models/estudiante.model';
 import { EstudianteService } from '../../services/estudiante.service';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-estudiante',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './estudiante.component.html',
   styleUrl: './estudiante.component.scss'
 })
@@ -16,15 +17,22 @@ export class EstudianteComponent implements OnInit {
   constructor(private estudianteService: EstudianteService){}
 
   ngOnInit(): void {
-    this.cargarEstudiante();
+    this.cargarEstudiantes();
   }
 
-  cargarEstudiante(): void{
+  cargarEstudiantes(): void{
     this.estudianteService.getEstudiantes().subscribe({
       next: (data) => this.estudiantes = data,
       error: (err) => console.error('Error al cargar estudiante', err)
     })
-
-    
   }
+
+    eliminarEstudiante(id: number): void{
+      if(confirm('Esta seguro de eliminar este estudiante')) {
+        this.estudianteService.deleteEstudiante(id).subscribe(() => {
+          this.cargarEstudiantes();
+        })
+      }
+    }
+  
 }
