adebaad42d1830044f6c5877ffa78f94a02f6e1c
Hanny Paz <hwpazz@gmail.com>
2025-06-08 11:35:25 -0500

    se agregar el get,put,post a servicios de profesor

--- src/app/services/profesor.service.ts
+++ src/app/services/profesor.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Profesor } from '../models/profesor.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfesorService {
+private apiUrl = 'https://localhost:44337/api/Profesores';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProfesores(): Observable<Profesor[]> {
+    return this.http.get<Profesor[]>(this.apiUrl);
+  }
+
+  getProfesor(id: number): Observable<Profesor> {
+    return this.http.get<Profesor>(`${this.apiUrl}/${id}`);
+  }
+
+  createProfesor(profesor: Profesor): Observable<Profesor> {
+    return this.http.post<Profesor>(this.apiUrl, profesor);
+  }
+
+  updateProfesor(id: number, profesor: Profesor): Observable<Profesor> {
+    return this.http.put<Profesor>(`${this.apiUrl}/${id}`, profesor);
+  }
+
+  deleteProfesor(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
